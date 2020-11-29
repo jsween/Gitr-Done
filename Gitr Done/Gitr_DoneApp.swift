96dd5d7ea30eb62b3bad7c84780567bf16d18851
Jon Sweeney <jonsweeney26@gmil.com>
2020-11-29 13:17:45 -0800

    Initial Commit

---
+++ Gitr Done/Gitr_DoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gitr_DoneApp.swift
+//  Gitr Done
+//
+//  Created by Jonathan Sweeney on 11/29/20.
+//
+
+import SwiftUI
+
+@main
+struct Gitr_DoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
